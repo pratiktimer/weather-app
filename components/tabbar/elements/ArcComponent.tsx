294796d0711b1309b0358f5e027fbc2265976191
praban <pbanawalkar2@gmail.com>
2025-11-03 21:28:53 +0530

    30. Tab Bar Part 6 - Border Stroke

--- components/tabbar/elements/ArcComponent.tsx
+++ components/tabbar/elements/ArcComponent.tsx
@@ -7,16 +7,21 @@ interface ArcComponentsProps {
 }
 const ArcComponent = ({ height, width }: ArcComponentsProps) => {
     const arcPath = `M 0 0 Q ${width / 2} ${height / 2} ${width} 0 L ${width} ${height}
-    L 0 ${height} Zr`;
+    L 0 ${height} Z`;
+    const arcBorder = `M 0 0 Q ${width / 2} ${height / 2} ${width} 0`;
     return (
         <Canvas style={{ height: height, width: width }}>
             <Path path={arcPath} >
                 <LinearGradient
                     start={vec(width / 2, 0)}
                     end={vec(width / 2, height)}
-                    colors={['rgba(58,58,106,1)', 'rgba(37,36,76,01)']}>
-
-                </LinearGradient>
+                    colors={['rgba(58,58,106,0.26)', 'rgba(37,36,76,0.26)']} />
+            </Path>
+            <Path
+                path={arcBorder}
+                style={"stroke"}
+                strokeWidth={0.5}
+                color={"rgba(117,130,244,0.5"}>
 
             </Path>
         </Canvas>
