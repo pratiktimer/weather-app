1b5eeff7b76e90631d2fbfd5b62b97da38969161
praban <pbanawalkar2@gmail.com>
2025-11-03 21:22:01 +0530

    29. Tab Bar - Part 5 - Linear Gradient

--- components/tabbar/elements/ArcComponent.tsx
+++ components/tabbar/elements/ArcComponent.tsx
@@ -1,16 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Canvas, Path } from '@shopify/react-native-skia';
+import { Canvas, LinearGradient, Path, vec } from '@shopify/react-native-skia';
 interface ArcComponentsProps {
     height: number;
     width: number
 }
 const ArcComponent = ({ height, width }: ArcComponentsProps) => {
     const arcPath = `M 0 0 Q ${width / 2} ${height / 2} ${width} 0 L ${width} ${height}
-    L 0 ${height} Z`;
+    L 0 ${height} Zr`;
     return (
         <Canvas style={{ height: height, width: width }}>
-            <Path path={arcPath} />
+            <Path path={arcPath} >
+                <LinearGradient
+                    start={vec(width / 2, 0)}
+                    end={vec(width / 2, height)}
+                    colors={['rgba(58,58,106,1)', 'rgba(37,36,76,01)']}>
+
+                </LinearGradient>
+
+            </Path>
         </Canvas>
     )
 }
