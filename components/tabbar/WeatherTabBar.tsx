17008af04aae6dcf0e1b92374f2f05277b6a936c
praban <pbanawalkar2@gmail.com>
2025-11-03 21:08:06 +0530

    26. Tab Bar - Part 2 - Position the Tabbar

--- components/tabbar/WeatherTabBar.tsx
+++ components/tabbar/WeatherTabBar.tsx
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ArcComponent from './elements/ArcComponent'
+import useApplicationDimensions from '../../hooks/useApplicationDimensions';
 
 const WeatherTabBar = () => {
+    const TabbarHeight = 88;
+    const { width, height } = useApplicationDimensions();
     return (
-        <View style={{ flex: 1, backgroundColor: 'red' }}>
-            <ArcComponent />
+        <View style={{
+            height: TabbarHeight, ...StyleSheet.absoluteFillObject,
+            top: height - TabbarHeight,
+        }}>
+            <ArcComponent height={TabbarHeight} width={width} />
         </View>
     )
 }
