3b66874b553e58ee02a751818041b110a3f7999c
praban <pbanawalkar2@gmail.com>
2025-11-03 21:48:25 +0530

    33. Tab Bar Part 9 - Middle Button

--- components/tabbar/elements/TabbarItems.tsx
+++ components/tabbar/elements/TabbarItems.tsx
@@ -2,8 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapIcon from '../icons/MapIcon'
 import ListIcon from '../icons/ListIcon'
+import TrapezoidBackground from './TrapezoidBackground'
+import useApplicationDimensions from '../../../hooks/useApplicationDimensions'
 
 const TabbarItems = () => {
+    const { width, height } = useApplicationDimensions();
+    const trapezoidWidth = width * 0.68;
+    const trapezoidHeight = height * 0.12;
     return (
         <View style={{
             flex: 1,
@@ -13,6 +18,7 @@ const TabbarItems = () => {
             paddingHorizontal: 32,
         }}>
             <MapIcon />
+            <TrapezoidBackground width={trapezoidWidth} height={trapezoidHeight} />
             <ListIcon />
         </View>
     )
