98ba0915116976a3f026efb8e392bdf8d9201f28
praban <pbanawalkar2@gmail.com>
2025-11-08 10:08:31 +0530

    54. Bottom Tab Bar Part 11 - Data !

--- components/forecast/ForecastCapsule.tsx
+++ components/forecast/ForecastCapsule.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import { Forecast } from '../../models/Weather'
 import { Canvas, RoundedRect, Shadow } from '@shopify/react-native-skia'
+import { DEGREE_SYMBOL } from '../../utils/Constants'
 
 interface ForecastCapsuleProps {
     forecast: Forecast,
@@ -12,6 +13,7 @@ interface ForecastCapsuleProps {
 }
 
 const ForecastCapsule = ({ forecast, width, height, radius }: ForecastCapsuleProps) => {
+    const { date, icon, probability, temperature } = forecast
     return (
         <View style={{ width: width, height: height }}>
             <Canvas style={{ flex: 1 }} >
@@ -23,6 +25,14 @@ const ForecastCapsule = ({ forecast, width, height, radius }: ForecastCapsulePro
                         inner />
 
                 </RoundedRect>
+                <View >
+                    <Text style={styles.timeText}>{date.toString()}</Text>
+                    <View>
+                        <Image source={icon} style={{ width: width / 2, height: width / 2 }} />
+                        <Text style={styles.probabilityText}>{probability}</Text>
+                    </View>
+                    <Text style={styles.temperatureText}>{temperature}{DEGREE_SYMBOL}</Text>
+                </View>
             </Canvas>
         </View>
     )
@@ -30,4 +40,25 @@ const ForecastCapsule = ({ forecast, width, height, radius }: ForecastCapsulePro
 
 export default ForecastCapsule
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    timeText: {
+        fontFamily: 'SF-Semibold',
+        fontSize: 15,
+        lineHeight: 20,
+        color: 'white',
+        letterSpacing: -0.5
+    },
+    probabilityText: {
+        fontFamily: 'SF-Semibold',
+        fontSize: 13,
+        lineHeight: 18,
+        color: '#40CBD8',
+    },
+    temperatureText: {
+        fontFamily: 'SF-Regular',
+        fontSize: 20,
+        lineHeight: 24,
+        color: 'white',
+        letterSpacing: 0.38
+    }
+})
