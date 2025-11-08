6efaec0472ea5eadca41b9efec2c715025ae87e3
praban <pbanawalkar2@gmail.com>
2025-11-08 10:20:17 +0530

    55. Bottom Tab Bar Part 12

--- components/forecast/ForecastCapsule.tsx
+++ components/forecast/ForecastCapsule.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Forecast } from '../../models/Weather'
 import { Canvas, RoundedRect, Shadow } from '@shopify/react-native-skia'
 import { DEGREE_SYMBOL } from '../../utils/Constants'
+import { convertDateTo12HrFormat } from '../../utils/DateHelper'
 
 interface ForecastCapsuleProps {
     forecast: Forecast,
@@ -14,9 +15,11 @@ interface ForecastCapsuleProps {
 
 const ForecastCapsule = ({ forecast, width, height, radius }: ForecastCapsuleProps) => {
     const { date, icon, probability, temperature } = forecast
+    const timeToDisplay = convertDateTo12HrFormat(date)
+
     return (
         <View style={{ width: width, height: height }}>
-            <Canvas style={{ flex: 1 }} >
+            <Canvas style={{ ...StyleSheet.absoluteFillObject }} >
                 <RoundedRect x={0} y={0} width={width} height={height} r={radius} color="rgba(72, 49, 157,1)" >
                     <Shadow dx={1}
                         dy={1}
@@ -25,15 +28,16 @@ const ForecastCapsule = ({ forecast, width, height, radius }: ForecastCapsulePro
                         inner />
 
                 </RoundedRect>
-                <View >
-                    <Text style={styles.timeText}>{date.toString()}</Text>
-                    <View>
-                        <Image source={icon} style={{ width: width / 2, height: width / 2 }} />
-                        <Text style={styles.probabilityText}>{probability}</Text>
-                    </View>
-                    <Text style={styles.temperatureText}>{temperature}{DEGREE_SYMBOL}</Text>
-                </View>
             </Canvas>
+            <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center', paddingVertical: 19 }} >
+                <Text style={styles.timeText}>{timeToDisplay}</Text>
+                <View>
+                    <Image source={icon} style={{ width: width / 2, height: width / 2 }} />
+                    <Text style={styles.probabilityText}>{probability}%</Text>
+                </View>
+                <Text style={styles.temperatureText}>{temperature}{DEGREE_SYMBOL}</Text>
+            </View>
+
         </View>
     )
 }
@@ -53,6 +57,7 @@ const styles = StyleSheet.create({
         fontSize: 13,
         lineHeight: 18,
         color: '#40CBD8',
+        textAlign: 'center',
     },
     temperatureText: {
         fontFamily: 'SF-Regular',
