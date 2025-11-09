7af1562fdd4c720b6ca5273f8e69f16350f7cc0d
praban <pbanawalkar2@gmail.com>
2025-11-09 09:50:35 +0530

    81. Details Page Part 4 - Search Bar!

--- screens/WeatherList.tsx
+++ screens/WeatherList.tsx
@@ -1,14 +1,17 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { use } from 'react'
 import BackgroundGradient from '../components/BackgroundGradient'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Feather } from '@expo/vector-icons';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Canvas, LinearGradient, RoundedRect, Shadow, vec } from '@shopify/react-native-skia';
+import useApplicationDimensions from '../hooks/useApplicationDimensions';
 const WeatherList = () => {
 
     const { top } = useSafeAreaInsets();
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+    const { width } = useApplicationDimensions();
 
     return (
         <>
@@ -26,6 +29,33 @@ const WeatherList = () => {
 
                     <Ionicons name='ellipsis-horizontal-circle' size={34} color={"white"}></Ionicons>
                 </View >
+                <View style={{ marginHorizontal: 16, borderRadius: 10, height: 36 }}>
+                    <Canvas style={{ ...StyleSheet.absoluteFillObject }}>
+                        <RoundedRect x={0} y={0} width={width - 32} height={36} r={10}>
+                            <LinearGradient
+                                start={vec(0, 0)}
+                                end={vec(width - 32, 36)}
+                                colors={["rgba(46,51,90,0.26)", "rgba(28,27,51,0.26)"]}
+                            />
+                            <Shadow dx={0} dy={4} blur={4} color={"rgba(0,0,0,1)"} inner />
+                        </RoundedRect>
+                    </Canvas>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                            paddingHorizontal: 8,
+                        }}
+                    >
+                        <Feather name="search" size={17} color={"rgba(235,235,246,0.6)"} />
+                        <TextInput
+                            placeholder="Search for a city or airport"
+                            placeholderTextColor={"rgba(235,235,246,0.6)"}
+                            style={styles.searchInput}
+                        />
+                    </View>
+                </View>
             </View>
         </>
     )
@@ -35,9 +65,16 @@ export default WeatherList
 
 const styles = StyleSheet.create({
     titleText: {
-        color: 'white',
-        fontFamily: 'SF-Semibold',
+        color: "white",
+        fontFamily: "SF-Semibold",
         fontSize: 28,
-        marginLeft: 34,
-    }
-})
+        lineHeight: 34,
+    },
+    searchInput: {
+        color: "rgba(235,235,246,0.5)",
+        fontFamily: "SF-Regular",
+        fontSize: 17,
+        lineHeight: 22,
+        paddingLeft: 10,
+    },
+});
