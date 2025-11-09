5cb85e0a0e17f84b058e6a109325fd37f66c655d
praban <pbanawalkar2@gmail.com>
2025-11-09 09:41:02 +0530

    80. Details Page Part 3 - Header

--- screens/WeatherList.tsx
+++ screens/WeatherList.tsx
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { use } from 'react'
 import BackgroundGradient from '../components/BackgroundGradient'
-
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 const WeatherList = () => {
+
+    const { top } = useSafeAreaInsets();
+    const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+
     return (
-        <BackgroundGradient />
+        <>
+            <BackgroundGradient />
+            <View style={{ flex: 1, paddingTop: top + 2, }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 16, paddingBottom: 8, }}>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Pressable onPress={() => navigation.navigate('home')}>
+                            <Ionicons name="chevron-back-sharp" size={34} color="rgba(235,235,245,0.6)" />
+                        </Pressable>
+
+                        <Text style={styles.titleText}>Weather</Text>
+                    </View>
+
+                    <Ionicons name='ellipsis-horizontal-circle' size={34} color={"white"}></Ionicons>
+                </View >
+            </View>
+        </>
     )
 }
 
 export default WeatherList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    titleText: {
+        color: 'white',
+        fontFamily: 'SF-Semibold',
+        fontSize: 28,
+        marginLeft: 34,
+    }
+})
